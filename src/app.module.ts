f067fb8af43b01f9d373b800a02d39c66cac8e6a
Minh <minzdapoet@gmail.com>
2025-07-26 00:13:56 +0700

    Initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
+
+import { Inject, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose'; // Assuming you have a MongoDB connection setup
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
+  imports: [
+    // imports: [MongooseModule.forRoot('mongodb+srv://BulkyMike:E3ooLx2M9HnPwfBd@cluster0.wlzqjty.mongodb.net/'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+
+    ConfigModule.forRoot({
+      isGlobal: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
